1ea6f8c236035a7fd67f0b3c069d62a0cb7b52d4
jadennatha <144076853+jadennatha@users.noreply.github.com>
2023-09-26 09:49:34 +0700

    jobsheet 5

---
+++ PemilihanPercobaan2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PemilihanPercobaan2 {
+    public static void main(String [] args){
+        Scanner input16 = new Scanner(System.in);
+
+        System.out.print("Nilai uas   : ");
+        float uas = input16.nextFloat();
+        System.out.print("Nilai uts   : ");
+        float uts = input16.nextFloat();
+        System.out.print("Nilai kuis  : ");
+        float kuis = input16.nextFloat();
+        System.out.print("Nilai tugas : ");
+        float tugas = input16.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        System.out.println("Nilai akhir = " + total + " Sehingga " + message);
+    }
+    
+}
