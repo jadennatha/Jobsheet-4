33ec2e94d56838b400357741125c5a402b59f325
jadennatha <144076853+jadennatha@users.noreply.github.com>
2023-10-03 08:40:50 +0700

    Jobsheet6

---
+++ Pemilihan2Percobaan116.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan116 {
+    public static void main(String [] args){
+        Scanner input16 = new Scanner(System.in);
+        
+        int tahun;
+
+        System.out.println("Masukkan tahun");
+        tahun = input16.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun & 100) != 0)
+            System.out.println("tahun Kabisat");
+            
+        }else {
+            System.out.println("bukan tahun kabisat");
+        }
+}
+}
