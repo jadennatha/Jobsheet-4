f75ca286869aadd6789e477f3b2a20d9f6cbf801
jadennatha <144076853+jadennatha@users.noreply.github.com>
2023-10-10 09:51:27 +0700

    Update ForKelipatan16.java

--- ForKelipatan16.java
+++ ForKelipatan16.java
@@ -4,6 +4,7 @@ public class ForKelipatan16 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+        int ratarata = 0;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) :");
         kelipatan = scan.nextInt();
@@ -11,9 +12,11 @@ public class ForKelipatan16 {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+                ratarata = jumlah/counter;
             }
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, ratarata);
  }
 }
